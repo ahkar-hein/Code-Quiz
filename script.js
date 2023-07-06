af71aaa3b210402e798fba04abf0655e1bd98b6c
Ahkar Hein <ahkarhein04@gmail.com>
2023-07-05 20:06:26 -0700

    added quiz question in javascript

---
+++ script.js
@@ -0,0 +1,37 @@
+//pseudocode
+// Create object for Quiz Question.
+// Get reference from HTML by using document.getElementbyID.
+// Start Quiz Function
+// Display Quiz Function
+// Timer
+// substract timer for wrong answer
+// Check Answer
+// Save Highscore localstorage
+
+const quizData = [
+    {
+        question: "HTML stands for -",
+        choices: ["HighText Machine Language", "HyperText and links Markup Language", "HyperText Markup Language", "None of these"],
+        answer: "HyperText Markup Language"
+    },
+    {
+        question: "The correct sequence of HTML tags for starting a webpage is -",
+        choices: ["Head, Title, HTML, body", "HTML, Body, Title, Head", "HTML, Head, Title, Body", "HTML, Head, Title, Body"],
+        answer: "HTML, Head, Title, Body"
+    },
+    {
+        question: "Which of the following element is responsible for making the text bold in HTML?",
+        choices: ["<pre>", "<a>", "<b>", "<br>"],
+        answer: "<b>"
+    },
+    {
+        question: "Which of the following tag is used for inserting the largest heading in HTML?",
+        choices: ["<h3>", "<h1>", "<h5>", "<h6>"],
+        answer: "<h1>"
+    },
+    {
+        question: "How to create an unordered list (a list with the list items in bullets) in HTML?",
+        choices: ["<ul>", "<ol>", "<li>", "<i>"],
+        answer: "<ul>"
+    },
+]
